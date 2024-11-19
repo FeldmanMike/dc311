3cc987a6c95f39f655dac8d8f76374dd822aedef
Mike Feldman <mike@mikefeldman.me>
2024-11-19 18:34:18 -0500

    Add new tests for preprocessing

--- tests/data/conftest.py
+++ tests/data/conftest.py
@@ -5,6 +5,8 @@ Set up fixtures for testing
 import json
 import os
 import pytest
+
+import pandas as pd
 import yaml
 
 
@@ -61,3 +63,19 @@ def test_json(scope="session"):
     json_path = os.path.join(os.path.dirname(__file__), "test.json")
     with open(json_path, "w") as file:
         json.dump(test_list, file)
+
+
+@pytest.fixture
+def basic_dataframe():
+    data_dict = {"COLUMN1": [1, 2, 3], "cOlUmN2": [4, 5, 6], "column3": [7, 8, 9]}
+    return pd.DataFrame(data_dict)
+
+
+@pytest.fixture
+def datetime_conversion_df():
+    data_dict = {
+        "date_col_1": [1609479125000, 1609481125000],
+        "date_col_2": [1609512550000, 1609509375000],
+        "col_3": [1, 2],
+    }
+    return pd.DataFrame(data_dict)
--- tests/data/test_preprocess.py
+++ tests/data/test_preprocess.py
@@ -3,11 +3,11 @@ Test dc311/data/extract.py
 """
 
 import os
-import pytest
 
 import pandas as pd
+from pandas.api.types import is_datetime64_any_dtype as is_datetime
 
-import dc311.data.preprocess as preprocess
+import dc311.data.preprocess as prep
 
 
 def test_json_to_csv_conversion(test_json):
@@ -15,7 +15,7 @@ def test_json_to_csv_conversion(test_json):
         curr_dir = os.path.dirname(__file__)
         json_path = os.path.join(curr_dir, "test.json")
         csv_path = os.path.join(curr_dir, "test.csv")
-        preprocess.transform_json_to_csv(json_path, csv_path)
+        prep.transform_json_to_csv(json_path, csv_path)
 
         assert os.path.exists(csv_path)
         df = pd.read_csv(csv_path)
@@ -29,3 +29,17 @@ def test_json_to_csv_conversion(test_json):
             os.remove(csv_path)
         if os.path.exists(json_path):
             os.remove(json_path)
+
+
+def test_transform_column_names_to_lowercase(basic_dataframe):
+    df = prep.transform_column_names_to_lowercase(basic_dataframe)
+    assert set(df.columns) == set(["column1", "column2", "column3"])
+
+
+def test_convert_columns_to_datetime(datetime_conversion_df):
+    df = prep.convert_columns_to_datetime(
+        datetime_conversion_df, ["date_col_1", "date_col_2"]
+    )
+    assert is_datetime(df["date_col_1"])
+    assert is_datetime(df["date_col_2"])
+    assert not is_datetime(df["col_3"])
