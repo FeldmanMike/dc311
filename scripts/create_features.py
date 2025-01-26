4c4abd2e0dffefc2f53cfbe83af6271bb4d713ff
Mike Feldman <mike@mikefeldman.me>
2025-01-26 12:01:30 -0500

    Reformat features script

--- scripts/create_features.py
+++ scripts/create_features.py
@@ -40,24 +40,27 @@ def main():
         data_path = args.input
     else:
         project_dir = os.path.dirname(os.path.dirname(__file__))
-        data_path = os.path.join(project_dir, "data", "interim",
-                                 "dc_311_preprocessed_data.csv")
+        data_path = os.path.join(
+            project_dir, "data", "interim", "dc_311_preprocessed_data.csv"
+        )
     logger.info(f"Data to preprocess is saved in directory: {data_path}")
 
     out_file_dir = os.path.join(project_dir, "data", "processed")
     dc311_df = pd.read_csv(data_path)
 
-    feature_pipe = feat.create_feature_engineering_pipeline(config['features'])
+    feature_pipe = feat.create_feature_engineering_pipeline(config["features"])
     feature_df = feature_pipe.fit_transform(dc311_df)
-    
-    # TODO - change preprocessing of days_to_resolve so that it is an integer
-    target_pipe = feat.create_target(
-                    df=dc311_df
-                    target_column=
-                    task=
-                    clf_threshold=
-                  )
 
+    # TODO - change preprocessing of days_to_resolve so that it is an integer
+
+
+#    target_pipe = feat.create_target(
+#                    df=dc311_df
+#                    target_column=
+#                    task=
+#                    clf_threshold=
+#                  )
+#
 
 if __name__ == "__main__":
     main()
