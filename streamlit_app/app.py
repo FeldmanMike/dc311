cdd6d622e8ee8e6bc79e23148d7dfcafd46edef1
Mike Feldman <mike@mikefeldman.me>
2025-05-22 20:42:11 -0400

    Debug streamlit app

--- streamlit_app/app.py
+++ streamlit_app/app.py
@@ -2,7 +2,7 @@
 Develop Streamlit app
 """
 
-from datetime import date
+from datetime import date, datetime
 import json
 import os
 import os.path as osp
@@ -31,25 +31,28 @@ with open("streamlit_app/request_categories.json") as f:
 WARDS = [f"Ward {i}" for i in range(1, 9)]
 
 st.title("DC 311 Request Resolution Time Predictor")
-
-request_type = st.selectbox("Type of 311 Request", REQUEST_TYPES)
-service_code = REQUEST_TYPES["All Requests"]["request_type"]
+request_category = st.selectbox("311 Request Category", list(REQUEST_TYPES.keys()))
+request_type = st.selectbox("311 Request Type", REQUEST_TYPES[request_category])
 submission_date = st.date_input("Date Submitted", value=date.today())
+submission_time = st.time_input("Time Submitted")
 ward_str = st.selectbox("Ward", WARDS)
-ward = int(ward_str.replace("Ward ", ""))
 
 if st.button("Predict"):
+    submission_datetime = datetime.combine(submission_date, submission_time)
+    ward = int(ward_str.replace("Ward ", ""))
+    service_code = REQUEST_TYPES["All Requests"][request_type]
     input_df = pd.DataFrame(
         {"servicecode": [service_code], "ward": [ward], "adddate": [submission_date]}
     )
+    input_df["adddate"] = pd.to_datetime(input_df["adddate"])
 
     processed_df = feature_pipe.transform(input_df)
 
     # Probability that request will take > 21 days to resolve
-    preds_21_day = under_21_day_model.predict_proba(processed_df)[1]
+    preds_21_day = under_21_day_model.predict_proba(processed_df)[:, 1]
 
     # Probability that request will take < 5 days to resolve
-    preds_5_day = under_5_day_model.predict_proba(processed_df)[0]
+    preds_5_day = under_5_day_model.predict_proba(processed_df)[:, 0]
 
     st.subheader("Prediction Results")
     st.write(f"🟢 Probability resolved in < 5 days: **{preds_5_day[0]:.1%}**")
